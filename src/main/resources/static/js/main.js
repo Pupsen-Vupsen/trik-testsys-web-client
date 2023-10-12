bbf890b4582df49161a8bcd4a016d0a851ffa694
Roman Shishkin <st077017@student.spbu.ru>
2023-10-12 04:14:19 +0300

    Added main scripts

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,10 @@
+function copy(id) {
+    var copyText = document.getElementById(id);
+    var textArea = document.createElement("textarea");
+
+    textArea.value = copyText.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("Copy");
+    textArea.remove();
+}
